ac98fc04115cfa6d93374a0f00454b0435abe493
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-13 21:57:50 +0600

    math keywords

---
+++ math.js
@@ -0,0 +1,16 @@
+const min = Math.min(45, 51, 2, 77, 56, 89, 62, 122);
+console.log(min);
+const max = Math.max(45, 51, 2, 77, 56, 89, 62, 122);
+console.log(max);
+
+
+console.log(Math.PI);
+console.log(Math.abs(50-130));
+console.log(Math.round(4.65));
+console.log(Math.round(4.12));
+console.log(Math.ceil(4.91))
+console.log(Math.ceil(4.11));
+console.log(Math.floor(4.91));
+console.log(Math.floor(4.11));
+console.log(Math.random());
+console.log(Math.random()*100);
